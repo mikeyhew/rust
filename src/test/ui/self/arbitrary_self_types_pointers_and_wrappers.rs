ff6568000ad17f840c8c0e0153de272ad48428ff
Michael Hewson <michael@michaelhewson.ca>
2019-09-22 16:07:23 -0400

    modify the pointers and wrappers test

--- src/test/ui/self/arbitrary_self_types_pointers_and_wrappers.rs
+++ src/test/ui/self/arbitrary_self_types_pointers_and_wrappers.rs
@@ -1,5 +1,5 @@
 // run-pass
-#![feature(arbitrary_self_types, unsize, coerce_unsized, dispatch_from_dyn)]
+#![feature(arbitrary_self_types, unsize, coerce_unsized, dispatch_from_dyn, unsized_locals)]
 #![feature(rustc_attrs)]
 
 use std::{
@@ -7,26 +7,32 @@ use std::{
     marker::Unsize,
 };
 
-struct Ptr<T: ?Sized>(Box<T>);
+struct Ptr<T: ?Sized> {
+    bx: Box<T>,
+    extra: i32,
+}
 
 impl<T: ?Sized> Deref for Ptr<T> {
     type Target = T;
 
     fn deref(&self) -> &T {
-        &*self.0
+        &*self.bx
     }
 }
 
 impl<T: Unsize<U> + ?Sized, U: ?Sized> CoerceUnsized<Ptr<U>> for Ptr<T> {}
 impl<T: Unsize<U> + ?Sized, U: ?Sized> DispatchFromDyn<Ptr<U>> for Ptr<T> {}
 
-struct Wrapper<T: ?Sized>(T);
+struct Wrapper<T: ?Sized>{
+    extra: i32,
+    value: T,
+}
 
 impl<T: ?Sized> Deref for Wrapper<T> {
     type Target = T;
 
     fn deref(&self) -> &T {
-        &self.0
+        &self.value
     }
 }
 
@@ -35,34 +41,67 @@ impl<T: DispatchFromDyn<U>, U> DispatchFromDyn<Wrapper<U>> for Wrapper<T> {}
 
 
 trait Trait {
-    // This method isn't object-safe yet. Unsized by-value `self` is object-safe (but not callable
-    // without unsized_locals), but wrappers arond `Self` currently are not.
-    // FIXME (mikeyhew) uncomment this when unsized rvalues object-safety is implemented
-    // fn wrapper(self: Wrapper<Self>) -> i32;
+    fn wrapper(self: Wrapper<Self>) -> i32;
     fn ptr_wrapper(self: Ptr<Wrapper<Self>>) -> i32;
     fn wrapper_ptr(self: Wrapper<Ptr<Self>>) -> i32;
     fn wrapper_ptr_wrapper(self: Wrapper<Ptr<Wrapper<Self>>>) -> i32;
 }
 
 impl Trait for i32 {
+    fn wrapper(self: Wrapper<Self>) -> i32 {
+        self.extra + *self
+    }
     fn ptr_wrapper(self: Ptr<Wrapper<Self>>) -> i32 {
-        **self
+        self.extra + (*self).extra + **self
     }
     fn wrapper_ptr(self: Wrapper<Ptr<Self>>) -> i32 {
-        **self
+        self.extra + (*self).extra + **self
     }
     fn wrapper_ptr_wrapper(self: Wrapper<Ptr<Wrapper<Self>>>) -> i32 {
-        ***self
+        self.extra + (*self).extra + (**self).extra + ***self
     }
 }
 
 fn main() {
-    let pw = Ptr(Box::new(Wrapper(5))) as Ptr<Wrapper<dyn Trait>>;
+    // FIXME (mikeyhew) using *Box::new(...) to get an unsized rvalue
+    // unsize the value directly once that's implemented
+    let w = *(Box::new(Wrapper {
+        extra: 1,
+        value: 2,
+    }) as Box<Wrapper<dyn Trait>>);
+
+    assert_eq!(w.wrapper(), 3);
+
+    let pw = Ptr {
+        extra: 2,
+        bx: Box::new(Wrapper {
+            extra: 1,
+            value: 2,
+        }),
+    } as Ptr<Wrapper<dyn Trait>>;
+
     assert_eq!(pw.ptr_wrapper(), 5);
 
-    let wp = Wrapper(Ptr(Box::new(6))) as Wrapper<Ptr<dyn Trait>>;
+    let wp = Wrapper {
+        extra: 3,
+        value: Ptr {
+            extra: -2,
+            bx: Box::new(5),
+        },
+    } as Wrapper<Ptr<dyn Trait>>;
+
     assert_eq!(wp.wrapper_ptr(), 6);
 
-    let wpw = Wrapper(Ptr(Box::new(Wrapper(7)))) as Wrapper<Ptr<Wrapper<dyn Trait>>>;
+    let wpw = Wrapper {
+        extra: 1,
+        value: Ptr {
+            extra: 2,
+            bx: Box::new(Wrapper {
+                extra: -4,
+                value: 8,
+            })
+        },
+    } as Wrapper<Ptr<Wrapper<dyn Trait>>>;
+
     assert_eq!(wpw.wrapper_ptr_wrapper(), 7);
 }
